ed7e05b448a32b48db6b6451b37dd5ea439f497a
Fangda Li <lifangda02@gmail.com>
2017-04-19 00:36:55 -0400

    got rid of sklearn functions

--- hw5/kmeans.py
+++ hw5/kmeans.py
@@ -7,6 +7,9 @@ class KMeans(object):
 		super(KMeans, self).__init__()
 		self.n_clusters = n_clusters
 		self.max_iter = max_iter
+		self.WC_SSD = 0.
+		self.SC = 0.
+		self.NMI = 0.
 
 	def fit(self, X):
 		'''
@@ -17,22 +20,47 @@ class KMeans(object):
 		# Randomly select K samples as initial centers
 		perm = permutation(n_samples)[:self.n_clusters]
 		C = X[perm]
+		# Nearest neighbor function
+		nn = lambda x: argmin(norm(C - x, axis=1))
 		# Iterate 50 times
 		for i in xrange(50):
 			# Assign points to nearest cluster centers
-			NN = NearestNeighbors(n_neighbors=1, metric='euclidean').fit(C)
-			dist, ind = NN.kneighbors(X)
-			ind = ind.flatten()
+			# NN = NearestNeighbors(n_neighbors=1, metric='euclidean').fit(C)
+			# dist, ind = NN.kneighbors(X)
+			# ind = ind.flatten()
+			ind = apply_along_axis(nn, 1, X)
 			# Update the cluster centers
 			for k in range(self.n_clusters):
 				C[k] = mean(X[ind == k], axis=0)
+		# Compute the WC_SSD
+		self.WC_SSD = sum((X - C[ind])**2)
+		# Compute the SC
+		def sc(x):
+			'''
+				Compute SC for a single sample.
+			'''
+			dist = norm(X - x[:-1], axis=1)
+			# x[-1] is encoded as the ind of x
+			wc = mean(dist[ind == x[-1]])
+			bc = mean(dist[ind != x[-1]])
+			return (bc - wc) / max(bc, wc)
+		# Encode ind along with X
+		X_enc = hstack((X, ind.reshape(-1, 1)))
+		self.SC = mean(apply_along_axis(sc, 1, X_enc))
 		return ind
+
+	def get_evals(self):
+		'''
+			Return WC_SSD, SC, NMI.
+		'''
+		return self.WC_SSD, self.SC, self.NMI
 
 if __name__ == '__main__':
 	X = randn(1000,2)
 	n_clusters = 10
 	kmeans = KMeans(n_clusters)
 	ind = kmeans.fit(X)
+	print "WC_SSD, SC, NMI =", kmeans.get_evals()
 	colors = rand(n_clusters, 3)[ind, :]
 	scatter(X[:, 0], X[:, 1], c=colors, alpha=0.9, s=30)
 	show()
