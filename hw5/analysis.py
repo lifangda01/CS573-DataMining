49c42da8a8c0513524a1a8b0f9ce875a63255501
Fangda Li <lifangda02@gmail.com>
2017-04-26 16:28:35 -0400

    updated analysis and figures

--- hw5/analysis.py
+++ hw5/analysis.py
@@ -1,7 +1,7 @@
 from pylab import *
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 from sklearn.metrics import silhouette_score, mutual_info_score
-from kmeans import KMeans, wc_ssd, sc
+from kmeans import KMeans, wc_ssd, sc, nmi
 from pca import PCA
 
 def get_normalized_labels(y):
@@ -12,13 +12,46 @@ def get_normalized_labels(y):
 		y[y == l] = i
 	return y
 
-def A1():
+def generate_pca_embedding_files():
 	'''
-		Visualizing one digit.
+		Generate PCA embedding csv files for the experiments.
 	'''
 	raw = genfromtxt('digits-raw.csv', delimiter=',')
-	image = raw[randint(raw.shape[0])][2:].reshape(28, 28)
-	imshow(image, cmap='gray')
+	X = raw[:, 2:]
+	pca = PCA(10)
+	X_new = pca.fit_transform(X)
+	raw_new = hstack((raw[:,:2], X_new))
+	savetxt('digits-pca-embedding.csv', raw_new, delimiter=',')
+
+def generate_subset_files():
+	'''
+		Generate embedding subsets: 2467 and 67.
+	'''
+	# tSNE embedding
+	raw = genfromtxt('digits-embedding.csv', delimiter=',')
+	y = raw[:, 1]
+	raw_new = raw[logical_or(y == 2, logical_or(y == 4, logical_or(y == 6, y == 7)))]
+	savetxt('digits-embedding-2467.csv', raw_new, delimiter=',')
+	raw_new = raw[logical_or(y == 6, y == 7)]
+	savetxt('digits-embedding-67.csv', raw_new, delimiter=',')
+	# PCA embedding
+	raw = genfromtxt('digits-pca-embedding.csv', delimiter=',')
+	y = raw[:, 1]
+	raw_new = raw[logical_or(y == 2, logical_or(y == 4, logical_or(y == 6, y == 7)))]
+	savetxt('digits-pca-embedding-2467.csv', raw_new, delimiter=',')
+	raw_new = raw[logical_or(y == 6, y == 7)]
+	savetxt('digits-pca-embedding-67.csv', raw_new, delimiter=',')
+
+def A1():
+	'''
+		Visualizing one digit from each class.
+	'''
+	raw = genfromtxt('digits-raw-small.csv', delimiter=',')
+	for i in range(10):
+		ind = raw[:, 1] == i
+		r = randint(sum(ind))
+		subplot(2, 5, i + 1)
+		imshow(raw[ind][r, 2:].reshape(28, 28), cmap='gray')
 	show()
 
 def A2():
@@ -32,12 +65,15 @@ def A2():
 	scatter(emb[perm, 2], emb[perm, 3], c=colors, alpha=0.9, s=30)
 	show()
 
-def B1():
+def B1(pca=False):
 	'''
 		Plot WC_SSD and SC over K.
 	'''
 	K = [2, 4, 6, 8, 16, 32]
-	fnames = ['digits-embedding.csv', 'digits-embedding-2467.csv', 'digits-embedding-67.csv']
+	if pca:
+		fnames = ['digits-pca-embedding.csv', 'digits-pca-embedding-2467.csv', 'digits-pca-embedding-67.csv']
+	else:
+		fnames = ['digits-embedding.csv', 'digits-embedding-2467.csv', 'digits-embedding-67.csv']
 	wc_ssd_val = zeros((len(fnames), len(K)))
 	sc_val= zeros((len(fnames), len(K)))
 	for i, fname in enumerate(fnames):  
@@ -74,25 +110,27 @@ def B3():
 				kmeans = KMeans(n_clusters=k)
 				kmeans.fit(X)
 				wc_ssd_val[i, j, m], sc_val[i, j, m], _ = kmeans.get_evals()
+	save('B3_wc_ssd_val.npy', wc_ssd_val), save('B3_sc_val.npy', sc_val)
+	wc_ssd_val = load('B3_wc_ssd_val.npy')
+	sc_val = load('B3_sc_val.npy')
 	ssd_means = mean(wc_ssd_val, axis=2)
 	sc_means = mean(sc_val, axis=2)
 	ssd_std = std(wc_ssd_val, axis=2)
 	sc_std = std(sc_val, axis=2)
-	save('B3_wc_ssd_val.npy', wc_ssd_val), save('B3_sc.npy', sc)
 	# Plot WC_SSD
 	figure()
 	for i, fname in enumerate(fnames):
-		errorbar(K, ssd_means[i], ssd_std[i], label=fname)
+		errorbar(K, ssd_means[i], ssd_std[i], capsize=4, label=fname)
 	legend()
 	title('WC_SSD v.s. K')
 	figure()
 	for i, fname in enumerate(fnames):
-		errorbar(K, sc_means[i], sc_std[i], label=fname)
+		errorbar(K, sc_means[i], sc_std[i], capsize=4, label=fname)
 	legend()
 	title('SC v.s. K')
 	show()
 
-def B4():
+def B4(pca=False):
 	'''
 		Evaluate using NMI and visualize in 2D.
 	'''
@@ -111,6 +149,7 @@ def B4():
 		ind = ind[perm]
 		colors = rand(k, 3)[ind, :]
 		scatter(X[:, 0], X[:, 1], c=colors, alpha=0.9, s=30)
+	print fnames
 	print "NMI =", nmi
 	show()
 
@@ -166,8 +205,8 @@ def C3():
 	# Randomly select 10 images from each digit
 	X = zeros((100, 2))
 	y = zeros(100)
-	wc_ssd_val = zeros((len(methods), len(K)))
-	sc_val= zeros((len(methods), len(K)))
+	wc_ssd_val = zeros((len(methods), len(K), 10))
+	sc_val= zeros((len(methods), len(K), 10))
 	for i in range(10):
 		ind = raw[:, 1] == i
 		perm = permutation(sum(ind))[:10]
@@ -176,31 +215,67 @@ def C3():
 	for i, m in enumerate(methods):
 		Z = linkage(X, method=m)
 		for j, k in enumerate(K):
-			ind = fcluster(Z, k, criterion='maxclust')
-			ind = get_normalized_labels(ind)
-			# Find the cluster centers
-			C = array([mean(X[ind == n], axis=0) for n in range(k)])
-			wc_ssd_val[i, j] = wc_ssd(X, C, ind)
-			sc_val[i, j] = sc(X, C, ind)
+			for l in range(10):
+				ind = fcluster(Z, k, criterion='maxclust')
+				ind = get_normalized_labels(ind)
+				# Find the cluster centers
+				C = array([mean(X[ind == n], axis=0) for n in range(k)])
+				wc_ssd_val[i, j, l] = wc_ssd(X, C, ind)
+				sc_val[i, j, l] = sc(X, C, ind)
+	ssd_means = mean(wc_ssd_val, axis=2)
+	sc_means = mean(sc_val, axis=2)
+	ssd_std = std(wc_ssd_val, axis=2)
+	sc_std = std(sc_val, axis=2)
 	figure()
 	for i, m in enumerate(methods):
-		plot(K, wc_ssd_val[i], label=m)
+		errorbar(K, ssd_means[i], ssd_std[i], capsize=4, label=m)
 	title('WC_SSD v.s. K')
 	xlabel('K')
 	ylabel('WC_SSD')
+	legend()
 	figure()
 	for i, m in enumerate(methods):
-		plot(K, sc_val[i], label=m)
+		errorbar(K, sc_means[i], sc_std[i], capsize=4, label=m)
 	title('SC v.s. K')
 	xlabel('K')
 	ylabel('SC')
+	legend()
+	show()
+
+def C5():
+	'''
+		NMI across distance measures.
+	'''
+
+	k = 8
+	methods = ['single', 'complete', 'average']
+	raw = genfromtxt('digits-embedding.csv', delimiter=',')
+	# Randomly select 10 images from each digit
+	X = zeros((100, 2))
+	y = zeros(100)
+	nmi_val = zeros((len(methods), 10))
+	for i in range(10):
+		ind = raw[:, 1] == i
+		perm = permutation(sum(ind))[:10]
+		X[10 * i: 10 * i + 10] = raw[ind, 2:][perm]
+		y[10 * i: 10 * i + 10] = raw[ind, 1][perm]
+	for i, m in enumerate(methods):
+		for j in range(10):
+			Z = linkage(X, method=m)
+			ind = fcluster(Z, k, criterion='maxclust')
+			ind = get_normalized_labels(ind)
+			nmi_val[i, j] = nmi(y, ind)
+	print "Average NMI across 10 trials:"
+	print methods
+	print mean(nmi_val, axis=1)
 	show()
 
 def Bonus2():
 	'''
 		Visualization of the first 10 eigen vectors.
 	'''
-	raw = genfromtxt('digits-raw.csv', delimiter=',')
+	# raw = genfromtxt('digits-raw.csv', delimiter=',')
+	raw = genfromtxt('digits-raw-small.csv', delimiter=',')
 	X = raw[:, 2:]
 	pca = PCA(10)
 	eigvec = pca.fit(X)
@@ -227,3 +302,9 @@ def Bonus3():
 	colors = rand(10, 3)[labels, :]
 	scatter(X_new[perm, 0], X_new[perm, 1], c=colors, alpha=0.9, s=10)
 	show()
+
+def Bonus4():
+	'''
+		Repeat B1, B2, B4 with PCA embedding.
+	'''
+	B1(pca=True)
--- hw5/pca.py
+++ hw5/pca.py
@@ -1,46 +1,62 @@
 from pylab import *
 from scipy.sparse.linalg import svds
+from sklearn.decomposition import PCA as skPCA
 
 class PCA(object):
 	'''
 		PCA modified from ECE661.
 	'''
-	def __init__(self, K):
+	def __init__(self, n_components):
 		super(PCA, self).__init__()
-		self.K = K
+		self.K = n_components
 		self.WK = None
 
 	def fit(self, X):
 		'''
-			Fit the model. X is num_samples x num_features.
+			Fit the model. 
+			X is n_samples x n_features.
 		'''
+		n_samples, n_features = X.shape
 		# Follow the notation of Avi's tutorial
-		X = X.T
-		m = mean(X, axis=1)
-		_, _, Ut = svds(dot(X.T, X), k=self.K)
-		W = dot(X, Ut.T)
+		# Center the data
+		X = X - mean(X, axis=0)
+		# X /= std(X, axis=0)
+		# Covariance matrix
+		C = dot(X.T, X) / (n_samples - 1.)
+		_, _, Ut = svd(C)
 		# Preserve the first K eigenvectors
-		# Each column is an eigenvector
-		self.WK = W[:,:self.K]
-		# Return the eigenvectors
-		return self.WK.T
+		# Each row is an eigenvector
+		self.WK = Ut[:self.K]
+		return self.WK
 
 	def transform(self, X):
 		'''
-			Transform the data. X is num_samples x num_features.
+			Transform the data. 
+			X is n_samples x n_features.
 		'''
-		X = X.T
-		m = mean(X, axis=1)
-		# Y is num_features x num_samples
-		Y = dot(self.WK.T, X - m.reshape(-1,1))
-		return Y.T
+		X = X - mean(X, axis=0)
+		# Y is n_features x n_samples
+		return dot(X, self.WK.T)
 
 	def fit_transform(self, X):
 		'''
 			Project samples onto the subspace spanned by the
 			first K eigenvectors of the covariance matrix.
 			Each sample is a row vector (same with sklearn).
-			Return num_samples x K.
+			Return n_samples x K.
 		'''
 		self.fit(X)
 		return self.transform(X)
+
+def main():
+	set_printoptions(precision=3, suppress=True)
+	X = randn(5, 3)
+	pca = PCA(n_components=2)
+	print pca.fit_transform(X)
+	print pca.fit(X)
+	skpca = skPCA(n_components=2)
+	print skpca.fit_transform(X)
+	print skpca.components_
+
+if __name__ == '__main__':
+	main()
